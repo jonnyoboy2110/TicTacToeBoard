0b831108e3aa86666476124e3bcbaf9cf2d21474
Jonathan Olvera <jonnyoboy2110@yahoo.com>
2018-11-28 12:06:13 -0800

    Made Functions

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,20 +19,34 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  if (turn == X)
+    return O ;
+  else
+  return X;
 }
 
 /**
  * Places the piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. placePiece does 
+ * piece is placed, and toggles which Piece's turn it is. placePiece does
  * NOT allow to place a piece in a location where there is already a piece.
- * In that case, placePiece just returns what is already at that location. 
+ * In that case, placePiece just returns what is already at that location.
  * Out of bounds coordinates return the Piece Invalid value. When the game
  * is over, no more pieces can be placed so attempting to place a piece
  * should neither change the board nor change whose turn it is.
-**/ 
+**/
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
+  if(row <0 ||row>=3 || column <0 ||column>=3)
+  {
+    return Invalid;
+  }
+  if (board[row][column] = Blank){
+    board[row][column] = turn;
+    return turn;
+  }
+    if (board[row][column] = X ||board[row][column] = O){
+    return board[row][column];
+  }
   return Invalid;
 }
 
@@ -42,7 +56,11 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
+    if(row <0 ||row>=3 || column <0 ||column>=3)
+  {
   return Invalid;
+  }
+  return board[row][column];
 }
 
 /**
@@ -51,5 +69,30 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+    for(int i=0; i<BOARDSIZE; i++){
+      for(int j=0; j<BOARDSIZE; j++){
+        if(board[i][j] == Blank;)
+          return Invalid;
+      }
+
+    }
+    for(int i=0; i<BOARDSIZE; i++){
+      if(board[i][0]  == board[i][1] &&board[i][1]  == board[i][2] )
+      {
+        return board[i][0];
+      }
+      if(board[0][i]  == board[1][i] &&board[1][i]  == board[2][i] )
+      {
+        return board[0][i];
+      }
+    }
+          if(board[0][0]  == board[1][1] &&board[2][2]  == board[1][1] )
+      {
+        return board[0][0];
+      }
+      if(board[0][2]  == board[1][1] &&board[2][0]  == board[1][1] )
+      {
+        return board[1][1];
+      }
+
 }
